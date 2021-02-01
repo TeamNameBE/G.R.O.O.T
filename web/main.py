f52e07dfed1584a7341d2bc47b71b2d1b3c21d53
Morti <qstevens@hotmail.be>
2021-02-01 16:35:47 +0100

    [ADD] ia page

--- web/main.py
+++ web/main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/ia')
+def ia_home():
+    return render_template('ia.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
