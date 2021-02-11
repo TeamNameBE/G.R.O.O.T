ab55c52337103a02c72e56b5a08d44af356bc204
Minigrim0 <grimauflorent@gmail.com>
2021-02-12 00:09:29 +0100

    [Add] upload methods

--- web/main.py
+++ web/main.py
@@ -13,14 +13,59 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/ia')
+@app.route("/ia", methods=["GET", "POST"])
 def ia_home():
-    return render_template('ia.html')
+    if request.method == "GET":
+        return render_template("ia.html")
+
+    if "file" not in request.files:
+        flash("Aucun fichier n\'a été fourni")
+        return redirect(request.url)
+
+    file = request.files["file"]
+    if file.filename == "":
+        flash("Aucune image n\'a été envoyée")
+        return redirect(request.url)
+
+    if allowed_file(file.filename):
+        database = redis.Redis(host="localhost", port=6379, db=0)
+
+        ext = get_extension(file.filename)
+        filename = secrets.token_hex(6)
+        while database.get(filename) is not None:
+            filename = secrets.token_hex(6)
+
+        filename_full = "{}{}".format(filename, ext)
+
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename_full))
+        database.set(filename, filename_full)
+        return redirect(f"/results?job={filename}")
+
+    else:
+        flash("Allowed image types are -> png, jpg, jpeg")
+        return redirect(request.url)
+
+
+@app.route("/results", methods=["GET"])
+def results_view():
+    filename = request.args.get("job", None)
+    if filename is None or filename == "":
+        return redirect("/")
+    return render_template("waiting-screen.html", filename=filename)
+
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    database = redis.Redis(host="localhost", port=6379, db=0)
+    filename_full = database.get(filename)
+    if filename_full is None:
+        return redirect(url_for('static', filename='img/404.gif'), code=301)
+    return redirect(url_for('static', filename='media/' + filename_full.decode()), code=301)
 
 
 if __name__ == "__main__":
