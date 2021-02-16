975fa242c500d10eda1b568eb26e97fdf39508f6
mzavagli <mike2024@hotmail.be>
2021-02-16 17:26:05 +0100

    [UPD] 1st test on flowers

--- groot/train.py
+++ groot/train.py
@@ -1,31 +1,102 @@
 import tensorflow as tf
+from tensorflow.keras import layers
+import sys
+import matplotlib.pyplot as plt
 
 
-def train(nb_train=500):
-    mnist = tf.keras.datasets.mnist
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+def train(argv):
 
-    x_train = tf.keras.utils.normalize(x_train, axis=1)
-    x_test = tf.keras.utils.normalize(x_test, axis=1)
+    nb_train = int(argv[0])
+    batch_size = 32
+    img_height = 32
+    img_width = 32
+
+    data_dir = "data/flowers"
+    """
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size
+    )
+
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size
+    )
+        
+    num_class = len(train_ds.class_names)
+
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    """
+
+    (train_ds, train_lb), (test_ds, test_lb) = tf.keras.datasets.cifar10.load_data()
+
+    num_class = 10
+    
+    """
+    model = tf.keras.models.Sequential([
+        layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+        layers.Conv2D(16, 3, padding="same", activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, padding="same", activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding="same", activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation="relu"),
+        layers.Dense(num_class)
+    ])
+    """
 
     model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-    model.add(tf.keras.layers.Dense(units=512, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(units=258, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(units=126, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(10))
 
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-    model.fit(x_train, y_train, epochs=nb_train)
+    history = model.fit(train_ds, train_lb, validation_data=(test_ds, test_lb), epochs=nb_train)
 
-    loss, accuracy = model.evaluate(x_test, y_test)
+    # loss, accuracy = model.evaluate(train_ds, val_ds)
 
-    print(accuracy)
-    print(loss)
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(nb_train)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
 
     model.save("digit.model")
 
-
 if __name__ == "__main__":
-    train()
+    train(sys.argv[1:])
