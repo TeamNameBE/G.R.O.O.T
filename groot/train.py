1f41c1a2b905c72685fc2f9f50dfafc1327369b3
mzavagli <mike2024@hotmail.be>
2021-02-18 19:17:15 +0100

    [ADD] pictures randomization and dropout

--- groot/train.py
+++ groot/train.py
@@ -12,14 +12,14 @@ def train(argv):
     img_width = 128
 
     data_dir = "data/flowers"
-    
+
     train_ds = tf.keras.preprocessing.image_dataset_from_directory(
         data_dir,
         validation_split=0.2,
         subset="training",
         seed=123,
         image_size=(img_height, img_width),
-        batch_size=batch_size
+        batch_size=batch_size,
     )
 
     val_ds = tf.keras.preprocessing.image_dataset_from_directory(
@@ -28,72 +28,71 @@ def train(argv):
         subset="validation",
         seed=123,
         image_size=(img_height, img_width),
-        batch_size=batch_size
+        batch_size=batch_size,
     )
-        
+
     num_class = len(train_ds.class_names)
 
     AUTOTUNE = tf.data.AUTOTUNE
 
     train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
     val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-    
-    
-    model = tf.keras.models.Sequential([
-        layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-        layers.Conv2D(16, 3, padding="same", activation="relu"),
-        layers.MaxPooling2D(),
-        layers.Conv2D(32, 3, padding="same", activation="relu"),
-        layers.MaxPooling2D(),
-        layers.Conv2D(64, 3, padding="same", activation="relu"),
-        layers.MaxPooling2D(),
-        layers.Flatten(),
-        layers.Dense(128, activation="relu"),
-        layers.Dense(num_class)
-    ])
-    
-    """
-    train_ds, test_ds = train_ds / 255.0, test_ds / 255.0
 
-    model = tf.keras.models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(10))
-    """
-    model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+    model = tf.keras.models.Sequential(
+        [
+            layers.experimental.preprocessing.RandomFlip(
+                "horizontal", input_shape=(img_height, img_width, 3)
+            ),
+            layers.experimental.preprocessing.RandomRotation(0.1),
+            layers.experimental.preprocessing.RandomZoom(0.1),
+            layers.experimental.preprocessing.Rescaling(1.0 / 255),
+            layers.Conv2D(16, 3, padding="same", activation="relu"),
+            layers.MaxPooling2D(),
+            layers.Conv2D(32, 3, padding="same", activation="relu"),
+            layers.MaxPooling2D(),
+            layers.Conv2D(64, 3, padding="same", activation="relu"),
+            layers.MaxPooling2D(),
+            layers.Dropout(0.2),
+            layers.Flatten(),
+            layers.Dense(128, activation="relu"),
+            layers.Dense(num_class),
+        ]
+    )
+
+    model.compile(
+        optimizer="adam",
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=["accuracy"],
+    )
 
     history = model.fit(train_ds, validation_data=val_ds, epochs=nb_train)
 
     # loss, accuracy = model.evaluate(train_ds, train_lb)
 
-    acc = history.history['accuracy']
-    val_acc = history.history['val_accuracy']
+    acc = history.history["accuracy"]
+    val_acc = history.history["val_accuracy"]
 
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
 
     epochs_range = range(nb_train)
 
     plt.figure(figsize=(8, 8))
     plt.subplot(1, 2, 1)
-    plt.plot(epochs_range, acc, label='Training Accuracy')
-    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-    plt.legend(loc='lower right')
-    plt.title('Training and Validation Accuracy')
+    plt.plot(epochs_range, acc, label="Training Accuracy")
+    plt.plot(epochs_range, val_acc, label="Validation Accuracy")
+    plt.legend(loc="lower right")
+    plt.title("Training and Validation Accuracy")
 
     plt.subplot(1, 2, 2)
-    plt.plot(epochs_range, loss, label='Training Loss')
-    plt.plot(epochs_range, val_loss, label='Validation Loss')
-    plt.legend(loc='upper right')
-    plt.title('Training and Validation Loss')
+    plt.plot(epochs_range, loss, label="Training Loss")
+    plt.plot(epochs_range, val_loss, label="Validation Loss")
+    plt.legend(loc="upper right")
+    plt.title("Training and Validation Loss")
     plt.show()
 
     model.save("digit.model")
+
 
 if __name__ == "__main__":
     train(sys.argv[1:])
