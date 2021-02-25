a9693d4f9196f76d03fece1458799b830e9f4b0f
Minigrim0 <grimauflorent@gmail.com>
2021-02-25 01:08:49 +0100

    [Upd] cache in files

--- groot/train.py
+++ groot/train.py
@@ -39,8 +39,8 @@ def train(argv):
 
     AUTOTUNE = tf.data.AUTOTUNE
 
-    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    train_ds = train_ds.cache(filename=".train_cache").shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache(filename=".train_cache").prefetch(buffer_size=AUTOTUNE)
 
     model = tf.keras.models.Sequential(
         [
