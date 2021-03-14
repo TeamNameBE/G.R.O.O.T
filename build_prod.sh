22fcbe4712ea7837152bea9d8108e3c3d08c5eb2
Minigrim0 <grimauflorent@gmail.com>
2021-03-14 12:50:37 +0100

    [Fix] repo is public no more need for ssh key

--- build_prod.sh
+++ build_prod.sh
@@ -1,4 +1,4 @@
-GIT_SSH_COMMAND='ssh -i /home/ubuntu/.ssh/id_rsa_groot -o IdentitiesOnly=yes' git pull
+git pull
 
 docker-compose up -d --build
 docker-compose restart nginx  # Sometimes the connection between nginx and web fails
